ce25303496fd90489a0966c9549abd426de3ade0
mikeilab21 <miguel@ilab.digital>
2023-08-22 19:09:26 -0500

    mysql12

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mysql = require('mysql2');
 const cors = require('cors');
 
 const app = express();
-const port = 3000; 
+const port = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
