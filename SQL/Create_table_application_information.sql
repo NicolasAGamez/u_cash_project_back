876ffe289b053699f2290079de32bbd9276efa96
Nicolas Gamez <nicolas@ilab.digital>
2023-10-12 10:21:10 -0500

    ADD: Añadir Id para la tabla application_information

--- SQL/Create_table_application_information.sql
+++ SQL/Create_table_application_information.sql
@@ -3,12 +3,16 @@ USE u_cash_customers;
 
 -- Crear la tabla de creación de usuario
 CREATE TABLE application_information (
+	id INT AUTO_INCREMENT PRIMARY KEY,
     identification VARCHAR(30),
     name VARCHAR(60),
     email VARCHAR(100),
     cell VARCHAR(15),
     chosenPlan VARCHAR(255),
-    requiredAmount DECIMAL(20, 2)
+    requiredAmount DECIMAL(20, 2),
+    validated VARCHAR(5),
+    approval VARCHAR(5),
+    status VARCHAR(20)
 );
 
 -- Insertar los datos en la nueva tabla uniéndose las tablas de clientes y de información de la calculadora.
